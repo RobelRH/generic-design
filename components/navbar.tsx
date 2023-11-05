813ebc01b598b7b49fbe8b85ca0d19047e9247c3
RobelRH <robel.redda28@gmail.com>
2023-11-05 18:11:13 +0300

    flyout added

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,15 @@ import { Dialog } from '@headlessui/react'
 import { Bars3Icon } from '@heroicons/react/20/solid'
 import { XMarkIcon } from '@heroicons/react/24/outline'
 import React, { useState } from 'react'
+import {
+    ArrowPathIcon,
+    ChartPieIcon,
+    CursorArrowRaysIcon,
+    FingerPrintIcon,
+    SquaresPlusIcon,
+} from '@heroicons/react/24/outline'
+import { Fragment } from 'react'
+import { Popover, Transition } from '@headlessui/react'
 
 const listMenuItems = [
     { name: "Home", href: "#" },
@@ -11,6 +20,14 @@ const listMenuItems = [
     { name: "Product", href: "#" },
     { name: "Service", href: "#" },
     { name: "Contact", href: "#" },
+]
+
+const productList = [
+    { iconColor: 'text-teal-500', icon: ArrowPathIcon, title: "Software Development", description: "Get a better understanding of your traffic" },
+    { iconColor: 'text-green-500', icon: ChartPieIcon, title: "Web Development", description: "Speak directly to your customers" },
+    { iconColor: 'text-blue-500', icon: CursorArrowRaysIcon, title: "Mobile Development", description: "Your customers' data will be safe and secure" },
+    { iconColor: 'text-yellow-500', icon: FingerPrintIcon, title: "Deployment Containers", description: "Connect with third-party tools" },
+    { iconColor: 'text-purple-500', icon: SquaresPlusIcon, title: "Machine Learning", description: "Build strategic funnels that will convert" },
 ]
 
 const Navbar = () => {
@@ -34,13 +51,42 @@ const Navbar = () => {
                     </div>
                     {/* list of menus  */}
                     <div className='hidden lg:flex'>
-                        <ul className='flex space-x-4'>
-                            {
-                                listMenuItems.map((menu) => (
-                                    <li><a href={menu.href}>{menu.name}</a></li>
-                                ))
-                            }
-                        </ul>
+
+                        <Popover>
+                            <ul className='flex space-x-4'>
+                                {
+                                    listMenuItems.map((menu) => (
+                                        <div className=''>
+                                            {
+                                                menu.name === "Product" ?
+
+                                                    <Popover.Button>
+                                                        <li className='hover:text-teal-600 hover:font-semibold'><a href={menu.href}>{menu.name}</a></li>
+                                                    </Popover.Button> : <li className='hover:text-teal-600  hover:font-semibold'><a href={menu.href}>{menu.name}</a></li>}
+                                        </div>
+                                    ))
+                                }
+                            </ul>
+                            <Transition>
+                                <Popover.Panel className="absolute">
+                                    <div className='bg-white shadow-lg py-4 rounded-lg'>
+                                        {
+                                            productList.map((product) => (
+                                                <div className='flex py-2 space-x-3 p-4  hover:bg-gray-50'>
+                                                    <div>
+                                                        <product.icon className={`h-5 ${product.iconColor}`} />
+                                                    </div>
+                                                    <div>
+                                                        <h3 className='font-semibold'>{product.title}</h3>
+                                                        <p>{product.description}</p>
+                                                    </div>
+                                                </div>
+                                            ))
+                                        }
+                                    </div>
+                                </Popover.Panel>
+                            </Transition>
+                        </Popover>
                     </div>
                     {/* login or sign up  */}
                     <div className='hidden lg:flex lg:flex-1 justify-end'>
@@ -61,6 +107,16 @@ const Navbar = () => {
                                 <button onClick={() => { setOpenMobile(false) }}>
                                     <XMarkIcon className='h-6' />
                                 </button>
+                            </div>
+                            {/* list of menus  */}
+                            <div className='py-10 px-10'>
+                                <ul className='space-y-4'>
+                                    {
+                                        listMenuItems.map((menu) => (
+                                            <li className='hover:text-teal-600'><a href={menu.href}>{menu.name}</a></li>
+                                        ))
+                                    }
+                                </ul>
                             </div>
                         </div>
                     </Dialog.Panel>
